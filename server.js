5d6767c821338a248005271a3902ddc179978de5
ns2hades-del <ns2hades@gmail.com>
2026-05-11 01:47:54 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -8,44 +8,116 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static(__dirname));
+app.use(express.json());
 
+const usersFile = "users.json";
 const messagesFile = "messages.json";
+const groupsFile = "groups.json";
 
-// Charger anciens messages
-function loadMessages() {
+function loadData(file) {
     try {
-        const data = fs.readFileSync(messagesFile);
-        return JSON.parse(data);
-    } catch (error) {
+        return JSON.parse(fs.readFileSync(file));
+    } catch {
         return [];
     }
 }
 
-// Sauvegarder messages
-function saveMessages(messages) {
-    fs.writeFileSync(messagesFile, JSON.stringify(messages, null, 2));
+function saveData(file, data) {
+    fs.writeFileSync(file, JSON.stringify(data, null, 2));
 }
 
-let messages = loadMessages();
+let users = loadData(usersFile);
+let messages = loadData(messagesFile);
+let groups = loadData(groupsFile);
 
-io.on("connection", (socket) => {
-    console.log("Utilisateur connecté");
+let onlineUsers = [];
 
-    // Envoyer ancien historique au nouvel utilisateur
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+
+app.get("/groups", (req, res) => {
+    res.json(groups);
+});
+
+app.post("/register", (req, res) => {
+    const { username, password } = req.body;
+
+    const existingUser = users.find(
+        user => user.username === username
+    );
+
+    if (existingUser) {
+        return res.json({
+            success: false,
+            message: "Utilisateur déjà existant"
+        });
+    }
+
+    const newUser = {
+        username,
+        password,
+        bio: "",
+        avatar: ""
+    };
+
+    users.push(newUser);
+    saveData(usersFile, users);
+
+    res.json({ success: true });
+});
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    const user = users.find(
+        user =>
+            user.username === username &&
+            user.password === password
+    );
+
+    if (user) {
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
+});
+
+app.post("/create-group", (req, res) => {
+    const { groupName } = req.body;
+
+    if (!groups.includes(groupName)) {
+        groups.push(groupName);
+        saveData(groupsFile, groups);
+    }
+
+    res.json({ success: true });
+});
+
+io.on("connection", socket => {
     socket.emit("load messages", messages);
+    socket.emit("online users", onlineUsers);
 
-    socket.on("chat message", (msg) => {
+    socket.on("user connected", username => {
+        if (!onlineUsers.includes(username)) {
+            onlineUsers.push(username);
+        }
+
+        io.emit("online users", onlineUsers);
+    });
+
+    socket.on("chat message", msg => {
         messages.push(msg);
-        saveMessages(messages);
+        saveData(messagesFile, messages);
 
         io.emit("chat message", msg);
     });
 
     socket.on("disconnect", () => {
-        console.log("Utilisateur déconnecté");
+        io.emit("online users", onlineUsers);
     });
 });
 
 server.listen(3000, () => {
-    console.log("Serveur lancé sur http://localhost:3000");
+    console.log("Serveur lancé sur port 3000");
 });
